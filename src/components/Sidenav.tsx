fb2e43a3ba6b9dcdcd5dfca1273b123b7f2b9d05
Himanshu Ghode <ghodehimanshu453@gmail.com>
2026-02-26 12:50:03 +0530

    setting page added

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -408,7 +408,7 @@ export function Sidenav({ avatarUrl = '', userProfile, children }: SidenavProps)
           <div className="h-4" />
 
           <NavItem icon={UserPlus}    label="Team"            href="/team" collapsed={isCollapsed} />
-          <NavItem icon={Settings}    label="Settings"        collapsed={isCollapsed} />
+          <NavItem icon={Settings}    label="Settings"        href="/setting" collapsed={isCollapsed} />
           <NavItem icon={ShieldCheck} label="Administration"  collapsed={isCollapsed} />
         </div>
 
